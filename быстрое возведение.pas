16d738260af46c2137ccc28deb341bbee1da6493
INNO1337 <ficus111199@gmail.com>
2019-01-04 23:50:32 +1000

    added 1 algorithm

---
+++ быстрое возведение.pas
@@ -0,0 +1,19 @@
+﻿Program ebs;
+  var
+    n:integer;
+    s,p:double;
+    begin
+    n:=8;
+    s:=2;
+    p:=1;
+      while (n<>0) do begin
+         if n mod 2 = 1 then begin
+          p:=p*s;
+         end; 
+         s:=s*s;
+         n:=n div 2;
+      end;
+      writeln(p);
+    end.
+    
+
